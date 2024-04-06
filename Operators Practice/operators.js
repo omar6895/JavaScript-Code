ebad70cdb9911de32cd4f92d1424c7967af64b02
Omar Abaidullah <158310456+omar6895@users.noreply.github.com>
2024-04-06 09:28:20 +0500

    Add files via upload

--- Operators Practice/operators.js
+++ Operators Practice/operators.js
@@ -1,1 +1,70 @@
+//Arithmetic Operators
 
+// let a = 6;
+// let b = 4;
+
+// console.log("a =", a, "& b =", b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+
+//Urany Operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a, "b =", b);
+
+// console.log("--a =", --a);
+// console.log("a =", a);
+
+//Assignment opeartors
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a, "b=", b);
+
+// a **= 4;
+//
+// console.log("a  =", a);
+
+//Comparison Operators
+
+// let a = 5;
+// let b = 6;
+
+// console.log("a=", a, "b=", b);
+
+// console.log("5 <= 6", a <= b);
+
+// Logical Operators
+
+// let a = 6;
+
+// let b = 5;
+
+// cond1 = a > b;
+// cond2 = a === 6;
+
+// console.log("cond1 && cond2 =", cond1 && cond2);
+
+// let a = 6;
+
+// let b = 5;
+
+// cond1 = a > a;
+// cond2 = a === 5;
+
+// console.log("cond1 || cond2 =", cond1 || cond2);
+
+// let a = 6;
+// let b = 5;
+
+// cond1 = a < b;
+// cond2 = a === 6;
+
+// console.log("!(cond1 && cond2) =", !(cond1 && cond2));
