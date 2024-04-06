356447d74eebd13d053a8fec34f42aeb32c0b23d
Omar Abaidullah <158310456+omar6895@users.noreply.github.com>
2024-04-06 08:31:41 +0500

    Add files via upload

---
+++ Variables Const Practice/PracticeConst.js
@@ -0,0 +1,29 @@
+// const student = {
+//   username: "Tony Shark",
+//   age: 10,
+//   follower: 10000,
+//   following: 500,
+//   bio: "I m a Software Engineer",
+//   image: "https://avatars.githubusercontent.com/u/43999999?v=4",
+//   location: "Bangalore",
+// };
+
+// student["age"] = student["age"] + 1;
+// student["username"] = "Raj Kumar";
+// student["following"] = student["following"] + 10000;
+// student["location"] = "Chennai";
+
+// console.log(student["location"]);
+
+// const product = {
+//   name: "Parker Jotter Standard CT Ball Pen",
+//   color: "Black",
+//   rating: 5,
+//   deal: true,
+//   price: 1000,
+//   offer: 5,
+// };
+// product["color"] = "Red";
+// product["name"] = "Ball Pen";
+
+// console.log(product["name"]);
